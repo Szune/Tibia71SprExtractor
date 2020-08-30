84812f33a4ec0263279c4a1953de398715c5e1d5
Szune <erikiwarson@gmail.com>
2020-08-30 14:18:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+﻿# Tibia71SprExtractor
+Reverse engineered as part of a bigger project to learn more about the Tibia 7.1 client, mostly for fun.
+
+Images at the end of the readme show how the Tibia client reads a single sprite.
+
+### .spr structure
+- U32 Version
+- U16 Sprite count (SC)
+- U32[SC] sprite start byte positions in file
+- Sprite[SC] sprite structures
+
+### Sprite structure:
+- U8 Transparent R
+- U8 Transparent G
+- U8 Transparent B
+- U16 Byte length
+- PixelData[Byte length] Sequence of PixelData
+
+### PixelData structure:
+- U16 Amount of transparent pixels to skip ahead
+- U16 Amount of colorful pixels to write before next PixelData or EOF
+- Pixel[Colorful pixels]
+
+### Pixel structure:
+- U8 Red
+- U8 Green
+- U8 Blue
+
+### Reading Tibia.spr:
+Every sprite is 32x32.
+
+	Read U32 version
+	Read U16 SpriteCount
+    Loop i = 0..SpriteCount - 1
+		SpritePositions[i] = Read U32 byte pos of sprite in file
+	Loop i = 0..SpriteCount - 1 
+		If SpritePositions[i] < 1 Then Skip
+		TransparentRed   = Read U8
+		TransparentGreen = Read U8
+		TransparentBlue  = Read U8
+        SpriteEnd        = (Current byte position in file) + (Read U16 byte length)
+		Sprite           = Create new Bitmap
+		CurrentPixel     = 0
+		Loop while current byte position in file < SpriteEnd
+            TransparentPixelCount = Read U16
+			CurrentPixel         += TransparentPixelCount
+            ColorfulPixelCount    = Read U16
+			Loop j = 0..ColorfulPixelCount
+				Red   = Read U8
+				Green = Read U8
+				Blue  = Read U8
+				X = CurrentPixel % 32
+				Y = CurrentPixel / 32
+				WritePixel(Sprite, X, Y, Red, Green, Blue)
+
+### Credits
+@jo3bingham -- fairly certain I read one of their threads about Tibia file structures on an OT forum (although for another version of the client, it was still very helpful)
+
+Tibia.exe -- everything else
+
+### Tibia.exe reading single sprite:
+Initializating sprite:
+![Init sprite](images/explanation.png)
+
+Reading sprite:
+![Read sprite](images/explanation2.png)
---
+++ Tibia71SprExtractor/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+
+namespace Tibia71SpriteExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int spriteWidth = 32;
+            const int spriteHeight = 32;
+			Stream file;
+			try
+			{
+				Console.Write("Trying to read Tibia.spr...");
+				file = File.OpenRead("Tibia.spr");
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("\nFailed to open and read Tibia.spr");
+				return;
+			}
+			Console.WriteLine(" done.");
+			using var br = new BinaryReader(file);
+
+            try
+            {
+				Console.Write("Trying to create folder 'Sprites'...");
+                Directory.CreateDirectory("Sprites");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"\nFailed to create 'Sprites' folder: {ex}");
+                return;
+            }
+			Console.WriteLine(" done.");
+
+            var version = br.ReadUInt32();
+            var spriteCount = br.ReadUInt16();
+            Console.WriteLine($"Sprite version: {version}");
+            Console.WriteLine($"Found {spriteCount} sprites.");
+			Console.Write("Extracting sprites...");
+            var spritePositions = new uint[spriteCount];
+            for (var i = 0; i < spriteCount; i++)
+            {
+                spritePositions[i] = br.ReadUInt32();
+            }
+
+            for (var i = 0; i < spriteCount; i++)
+            {
+                var pos = spritePositions[i];
+                if (pos < 1) continue;
+                var transparentR = br.ReadByte(); // in case we want to draw the transparent colors as well
+                var transparentG = br.ReadByte();
+                var transparentB = br.ReadByte();
+                var spriteEnd = br.BaseStream.Position + br.ReadUInt16();
+                using (var sprite = new Bitmap(spriteWidth, spriteHeight))
+                {
+                    var sprData = sprite.LockBits(new Rectangle(0, 0, spriteWidth, spriteHeight),
+                        ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+                    var cPixel = 0;
+                    unsafe // need for speed
+                    {
+                        var bmpPtr = (byte*) sprData.Scan0;
+                        while (br.BaseStream.Position < spriteEnd)
+                        {
+                            var transparentPixels = br.ReadUInt16();
+                            var colorfulPixels = br.ReadUInt16();
+                            cPixel += transparentPixels;
+                            bmpPtr += transparentPixels * 4;
+                            for (var p = 0; p < colorfulPixels; p++)
+                            {
+                                var (red, green, blue) = (br.ReadByte(), br.ReadByte(), br.ReadByte());
+                                bmpPtr[0] = blue; // blue
+                                bmpPtr[1] = green; // green
+                                bmpPtr[2] = red; // red
+                                bmpPtr[3] = 255; // alpha
+                                bmpPtr += 4;
+                                cPixel++;
+                            }
+                        }
+                    }
+
+                    sprite.UnlockBits(sprData);
+                    sprite.Save($"Sprites/{i+1}.bmp");
+                }
+            }
+			Console.WriteLine(" done.");
+			Console.ReadLine();
+        }
+
+        static void PrintBytes(byte[] bytes)
+        {
+            Console.WriteLine(string.Join(" ", bytes.Select(b=>$"{b:X2}")));
+        }
+    }
+}
